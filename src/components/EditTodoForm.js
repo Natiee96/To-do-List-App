a3d74469cee8e0a984643214a5ce74aea3406aa3
Onatiee Ogbebor <onatieee@gmail.com>
2023-04-21 16:53:00 +0100

    Add files via upload

---
+++ src/components/EditTodoForm.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+
+export const EditTodoForm = ({editTodo, task}) => {
+  const [value, setValue] = useState(task.task)
+
+  const handleSumbit = e => {
+    e.preventDefault();
+
+    editTodo(value, task.id);
+
+    setValue("")
+  }
+  return (
+    <form className='TodoForm' onSubmit={handleSumbit}>
+      <input type='text' className='todo-input' value={value}
+      placeholder='Update Task' onChange=
+      {(e) => setValue(e.target.value)}/>
+      <button type='submit' 
+      className='todo-btn'>Update Task</button>
+
+    </form>  
+
+  )
+}
+
